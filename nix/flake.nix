af1f18ef8cd2aa82b786aa4f4f0020fce4179e26
Jesse Brizzi <jessebrizzi@gmail.com>
2025-01-12 13:07:45 -0500

    update

---
+++ nix/flake.nix
@@ -0,0 +1,77 @@
+{
+  description = "jesseb nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+  };
+
+  outputs = inputs@{ self, nix-darwin, nixpkgs, nix-homebrew }:
+  let
+    configuration = { pkgs, config, ... }: {
+      
+      nixpkgs.config.allowUnfree = true;
+      
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        [ pkgs.btop
+          pkgs.tmux
+          pkgs.tree
+        ];
+
+      homebrew = {
+        enable = true;
+        brews = [
+          "mas"
+        ];
+        casks = [
+          "the-unarchiver"
+          "vlc"
+        ];
+        masApps = {
+          "NordVPN" = 905953485;
+        };
+        onActivation.cleanup = "zap";
+      };
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Jesses-Air
+    darwinConfigurations."Jesses-Air" = nix-darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "jesse";
+          };
+        }
+      ];
+    };
+
+    # Expose the package set, including overlays, for convienience.
+    darwinPackages = self.darwinConfigurations."Jesses-Air".pkgs;
+  };
+}
